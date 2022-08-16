1ca350dc55c0e8cbfad91ade14d317f5cdcbc777
Mikecerc1 <mikecerc1@outlook.com>
2022-08-16 18:57:10 -0400

    whoops

--- Events/server/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ready',
-    execute(client) {
-        console.log('The client is ready :)');
-        client.user.setActivity('my spindexer breaking', { type: 'LISTENING' });
-    },
-};
